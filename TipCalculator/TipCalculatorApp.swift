36d0762574192395f639e2a75461de4a30b7d8d4
FrankSolleveld <fsolleveld@outlook.com>
2021-04-20 13:34:59 +0200

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Frank Solleveld on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
